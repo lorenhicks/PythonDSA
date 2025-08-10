9a7cf327ddbbaa483b7f8bf3b91c5f32d6ae480a
Loren Hicks <lorenhicks@gmail.com>
2025-08-10 18:10:08 -0400

    Insert Hash Code

--- hashtable.py
+++ hashtable.py
@@ -8,3 +8,10 @@ def hash_function(value):
     return sum_of_chars % 10
 
 print(" 'Bob' has hash code:", hash_function('Bob'))
+
+def add(name):
+    index = hash_function(name)
+    my_list[index] = name
+
+add('Bob')
+print(my_list)
