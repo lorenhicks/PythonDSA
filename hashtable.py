b63b77045d2dcdb5b42ae821518cbe410104fd69
Loren Hicks <lorenhicks@gmail.com>
2025-08-10 18:32:28 -0400

    Resolving Collisions

--- hashtable.py
+++ hashtable.py
@@ -1,4 +1,4 @@
-my_list = [None, None, None, None, None, None, None, None, None, None]
+my_list = [[], [], [], [], [], [], [], [], [], []]
 
 def hash_function(value):
     sum_of_chars = 0
@@ -11,13 +11,14 @@ print(" 'Bob' has hash code:", hash_function('Bob'))
 
 def add(name):
     index = hash_function(name)
-    my_list[index] = name
+    my_list[index].append(name)
 
 add('Bob')
 add('Pete')
 add('Jones')
 add('Lisa')
 add('Siri')
+add('Stuart')
 print(my_list)
 
 def contains(name):
