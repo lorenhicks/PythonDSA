0bdaac550d7ae19cb2c7385eda35a90e1782bfdf
Loren Hicks <lorenhicks@gmail.com>
2025-08-11 07:11:04 -0400

    AVL Tree Node Deletion

--- avltree.py
+++ avltree.py
@@ -16,7 +16,6 @@ def getBalance(node):
   return getHeight(node.left) - getHeight(node.right)
 
 def rightRotate(y):
-  print('Rotate right on node',y.data)
   x = y.left
   T2 = x.right
   x.right = y
@@ -26,7 +25,6 @@ def rightRotate(y):
   return x
 
 def leftRotate(x):
-  print('Rotate left on node',x.data)
   y = x.right
   T2 = y.left
   y.left = x
@@ -44,25 +42,19 @@ def insert(node, data):
   elif data > node.data:
     node.right = insert(node.right, data)
 
-  # Update the balance factor and balance the tree
   node.height = 1 + max(getHeight(node.left), getHeight(node.right))
   balance = getBalance(node)
 
-  # Balancing the tree
-  # Left Left
   if balance > 1 and getBalance(node.left) >= 0:
     return rightRotate(node)
 
-  # Left Right
   if balance > 1 and getBalance(node.left) < 0:
     node.left = leftRotate(node.left)
     return rightRotate(node)
 
-  # Right Right
   if balance < -1 and getBalance(node.right) <= 0:
     return leftRotate(node)
 
-  # Right Left
   if balance < -1 and getBalance(node.right) > 0:
     node.right = rightRotate(node.right)
     return leftRotate(node)
@@ -75,11 +67,69 @@ def inOrderTraversal(node):
   inOrderTraversal(node.left)
   print(node.data, end=", ")
   inOrderTraversal(node.right)
+    
+def minValueNode(node):
+  current = node
+  while current.left is not None:
+    current = current.left
+  return current
 
-# Inserting nodes
+def minValueNode(node):
+  current = node
+  while current.left is not None:
+    current = current.left
+  return current
+
+def delete(node, data):
+  if not node:
+    return node
+
+  if data < node.data:
+    node.left = delete(node.left, data)
+  elif data > node.data:
+    node.right = delete(node.right, data)
+  else:
+    if node.left is None:
+      temp = node.right
+      node = None
+      return temp
+    elif node.right is None:
+      temp = node.left
+      node = None
+      return temp
+
+    temp = minValueNode(node.right)
+    node.data = temp.data
+    node.right = delete(node.right, temp.data)
+
+  if node is None:
+    return node
+
+  node.height = 1 + max(getHeight(node.left), getHeight(node.right))
+  balance = getBalance(node)
+
+  if balance > 1 and getBalance(node.left) >= 0:
+    return rightRotate(node)
+
+  if balance > 1 and getBalance(node.left) < 0:
+    node.left = leftRotate(node.left)
+    return rightRotate(node)
+
+  if balance < -1 and getBalance(node.right) <= 0:
+    return leftRotate(node)
+
+  if balance < -1 and getBalance(node.right) > 0:
+    node.right = rightRotate(node.right)
+    return leftRotate(node)
+
+  return node
+
 root = None
 letters = ['C', 'B', 'E', 'A', 'D', 'H', 'G', 'F']
 for letter in letters:
   root = insert(root, letter)
 
+inOrderTraversal(root)
+print('\nDeleting A')
+root = delete(root,'A')
 inOrderTraversal(root)
